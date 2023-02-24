9dd4354ade15f641ecd5355238736a95ad928141
singleufo <ghoshabhirup2001@gmail.com>
2023-02-24 10:35:20 +0530

    Editing nav

--- 6_Forum/partials/_navbar.php
+++ 6_Forum/partials/_navbar.php
@@ -11,6 +11,7 @@ echo '
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex align-items-center justify-content-between w-100"> 
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="/phpw/6_Forum/">Home</a>
@@ -42,24 +43,25 @@ echo '
             </ul>';
 if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] = true) {
     echo '
-        <div class="container">
+        <div class="justify-content-end">
             <button type="button" class="btn btn-outline-light" disabled>Welcome ' . $_SESSION['userEmail'] . '</button>
             <a role="button" href="partials/_logout.php" class="btn btn-primary">Logout</a>
         </div>';
 } else {
     echo '
-        <div class="container">
+        <div class="justify-content-end">
             <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
             <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#signUpModal">Sign Up</button>
         </div>';
 }
 
 
-echo '<form class="form-inline d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+// echo '<form class="form-inline d-flex" role="search">
+//                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+//                 <button class="btn btn-outline-success" type="submit">Search</button>
 
-            </form>
+//             </form>
+echo '  </div>
         </div>
     </div>
 </nav>';
